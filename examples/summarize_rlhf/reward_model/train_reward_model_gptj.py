59488b925656ce72078fa7717f2b3395affdbeab
Ava <avajhye@gmail.com>
2023-02-26 21:46:09 +0800

    gpt2_version_0

--- examples/summarize_rlhf/reward_model/train_reward_model_gptj.py
+++ examples/summarize_rlhf/reward_model/train_reward_model_gptj.py
@@ -86,7 +86,8 @@ def compute_metrics(eval_preds):
 
 
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    # tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     tokenizer.pad_token = tokenizer.eos_token
 
     if not os.path.exists("rm_checkpoint"):
